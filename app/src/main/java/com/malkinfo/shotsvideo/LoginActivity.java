c43abd85b9e13ff5604424940c2f890955513516
TEPEI <teppei3877@gmail.com>
2022-05-20 10:35:01 +0900

    0520third

--- app/src/main/java/com/malkinfo/shotsvideo/LoginActivity.java
+++ app/src/main/java/com/malkinfo/shotsvideo/LoginActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        Button sendButton = findViewById(R.id.send_button);
+        // lambda式
+        sendButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplication(), MainActivity.class);
+            startActivity(intent);
     }
 }
