7cf4c4540f4e0c1bde268858c3488296340d24fb
ola <haya.g.bb@gmail.com>
2022-05-25 13:08:31 +0900

    VideoUp

--- app/src/main/java/com/malkinfo/shotsvideo/UploadingActivity.kt
+++ app/src/main/java/com/malkinfo/shotsvideo/UploadingActivity.kt
@@ -6,13 +6,14 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
+import android.widget.RelativeLayout
 
 class UploadingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_uploading)
 
-        val returnButton: Button = findViewById<Button>(R.id.returnFromSuccess)
+        val returnButton: RelativeLayout = findViewById<RelativeLayout>(R.id.success)
 
         returnButton.setOnClickListener { v: View? ->
             val intent = Intent(application, VideoUpActivity::class.java)
